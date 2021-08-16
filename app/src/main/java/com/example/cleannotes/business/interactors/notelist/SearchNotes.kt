327ab5c3d91b6ca1748e20daab3e5bfb1d804395
Shashank Sinha <sinhashashank.98@gmail.com>
2021-08-17 00:48:04 +0530

    SearchNotes Use-case Added

--- app/src/main/java/com/example/cleannotes/business/interactors/notelist/SearchNotes.kt
+++ app/src/main/java/com/example/cleannotes/business/interactors/notelist/SearchNotes.kt
@@ -1,4 +1,75 @@
 package com.example.cleannotes.business.interactors.notelist
 
-class SearchNotes {
+import com.example.cleannotes.business.data.cache.CacheResponseHandler
+import com.example.cleannotes.business.data.cache.CacheResult
+import com.example.cleannotes.business.data.cache.abstraction.NoteCacheDataSource
+import com.example.cleannotes.business.data.util.safeCacheCall
+import com.example.cleannotes.business.domain.model.Note
+import com.example.cleannotes.business.domain.state.*
+import com.example.cleannotes.framework.presentation.notelist.state.NoteListStateEvent
+import com.example.cleannotes.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+class SearchNotes(
+    private val noteCacheDataSource: NoteCacheDataSource
+) {
+
+    fun searchNotes(
+        query: String,
+        filterAndOrder: String,
+        page: Int,
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteListViewState>?> = flow {
+
+        var updatedPage = page
+        if(updatedPage<=0) updatedPage = 1
+
+        val cacheResult: CacheResult<List<Note>?> = safeCacheCall(IO) {
+            noteCacheDataSource.searchNotes(
+                query = query,
+                filterAndOrder= filterAndOrder,
+                page =  updatedPage
+            )
+        }
+
+        val response: DataState<NoteListViewState>? = object: CacheResponseHandler<NoteListViewState, List<Note>>(
+            response = cacheResult,
+            stateEvent= stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: List<Note>): DataState<NoteListViewState>? {
+
+                var message: String?= SEARCH_NOTES_SUCCESS
+                var uiComponentType: UIComponentType = UIComponentType.None()
+                if(resultObj.isEmpty()) {
+                   message = SEARCH_NOTES_NO_MATCHING_RESULTS
+                   uiComponentType = UIComponentType.Toast()
+                }
+
+                return DataState.data(
+                    response = Response(
+                        message = message,
+                        uiComponentType = uiComponentType,
+                        messageType = MessageType.Success()
+                    ),
+                    data = NoteListViewState(noteList = ArrayList(resultObj)),
+                    stateEvent = stateEvent
+                )
+            }
+
+        }.getResult()
+
+
+        emit(response)
+
+    }
+
+    companion object{
+        val SEARCH_NOTES_SUCCESS = "Successfully retrieved list of notes."
+        val SEARCH_NOTES_NO_MATCHING_RESULTS = "There are no notes that match that query."
+        val SEARCH_NOTES_FAILED = "Failed to retrieve the list of notes."
+    }
+
 }
--- app/src/test/java/com/example/cleannotes/business/interactors/notelist/InsertNewNoteTest.kt
+++ app/src/test/java/com/example/cleannotes/business/interactors/notelist/InsertNewNoteTest.kt
@@ -1,10 +1,50 @@
 package com.example.cleannotes.business.interactors.notelist
 
+import com.example.cleannotes.business.data.cache.CacheErrors
+import com.example.cleannotes.business.data.cache.FORCE_GENERAL_FAILURE
+import com.example.cleannotes.business.data.cache.FORCE_NEW_NOTE_EXCEPTION
 import com.example.cleannotes.business.data.cache.abstraction.NoteCacheDataSource
 import com.example.cleannotes.business.data.network.abstraction.NoteNetworkDataSource
 import com.example.cleannotes.business.domain.model.NoteFactory
+import com.example.cleannotes.business.domain.state.DataState
+import com.example.cleannotes.business.interactors.notelist.InsertNewNote.Companion.INSERT_NOTE_FAILED
+import com.example.cleannotes.business.interactors.notelist.InsertNewNote.Companion.INSERT_NOTE_SUCCESS
 import com.example.cleannotes.di.DependencyContainer
+import com.example.cleannotes.framework.presentation.notelist.state.NoteListStateEvent
+import com.example.cleannotes.framework.presentation.notelist.state.NoteListViewState
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.util.*
 
+
+/*
+Test cases:
+1. insertNote_success_confirmNetworkAndCacheUpdated()
+    a) insert a new note
+    b) listen for INSERT_NOTE_SUCCESS emission from flow
+    c) confirm cache was updated with new note
+    d) confirm network was updated with new note
+2. insertNote_fail_confirmNetworkAndCacheUnchanged()
+    a) insert a new note
+    b) force a failure (return -1 from db operation)
+    c) listen for INSERT_NOTE_FAILED emission from flow
+    e) confirm cache was not updated
+    e) confirm network was not updated
+3. throwException_checkGenericError_confirmNetworkAndCacheUnchanged()
+    a) insert a new note
+    b) force an exception
+    c) listen for CACHE_ERROR_UNKNOWN emission from flow
+    e) confirm cache was not updated
+    e) confirm network was not updated
+ */
+
+@InternalCoroutinesApi
 class InsertNewNoteTest {
 
     // system in test
@@ -28,6 +68,108 @@ class InsertNewNoteTest {
             noteFactory = noteFactory
         )
     }
+
+
+
+    // gonna call coroutines inside, so use runBlocking
+    @Test
+    fun insertNote_success_confirmNetworkAndCacheUpdated() = runBlocking {
+
+        val newNote = noteFactory.creatingSingleNote(
+            id= null,
+            title = UUID.randomUUID().toString()
+        )
+
+        insertNewNote.insertNewNote(
+            id= newNote.id,
+            title= newNote.title,
+            stateEvent = NoteListStateEvent.InsertNewNoteEvent(newNote.title)
+        ).collect(object : FlowCollector<DataState<NoteListViewState>?> {  // listening flow emission
+            override suspend fun emit(value: DataState<NoteListViewState>?) {
+                assertEquals(
+                    value?.stateMessage?.response?.message,
+                    INSERT_NOTE_SUCCESS
+                )
+            }
+        })
+
+
+        // confirm cache was updated
+        val cacheNoteThatWasInserted = noteCacheDataSource.searchNoteById(newNote.id)
+        assertTrue { cacheNoteThatWasInserted == newNote }
+
+        // confirm network was updated
+        val networkNoteThatWasInserted = noteNetworkDataSource.searchNote(newNote)
+        assertTrue { networkNoteThatWasInserted == newNote}
+
+    }
+
+
+
+    @Test
+    fun insertNote_fail_confirmNetworkAndCacheUnchanged() = runBlocking {
+
+        val newNote = noteFactory.creatingSingleNote(
+            id= FORCE_GENERAL_FAILURE,
+            title = UUID.randomUUID().toString()
+        )
+
+        insertNewNote.insertNewNote(
+            id= newNote.id,
+            title= newNote.title,
+            stateEvent = NoteListStateEvent.InsertNewNoteEvent(newNote.title)
+        ).collect(object : FlowCollector<DataState<NoteListViewState>?> {  // listening flow emission
+            override suspend fun emit(value: DataState<NoteListViewState>?) {
+                assertEquals(
+                    value?.stateMessage?.response?.message,
+                    INSERT_NOTE_FAILED
+                )
+            }
+        })
+
+
+        // confirm cache was not updated
+        val cacheNoteThatWasInserted = noteCacheDataSource.searchNoteById(newNote.id)
+        assertTrue { cacheNoteThatWasInserted == null }
+
+        // confirm network was not updated
+        val networkNoteThatWasInserted = noteNetworkDataSource.searchNote(newNote)
+        assertTrue { networkNoteThatWasInserted == null}
+
+    }
+
+    @Test
+    fun throwException_checkGenericError_confirmNetworkAndCacheUnchanged() = runBlocking {
+
+        val newNote = noteFactory.creatingSingleNote(
+            id= FORCE_NEW_NOTE_EXCEPTION,
+            title = UUID.randomUUID().toString()
+        )
+
+        insertNewNote.insertNewNote(
+            id= newNote.id,
+            title= newNote.title,
+            stateEvent = NoteListStateEvent.InsertNewNoteEvent(newNote.title)
+        ).collect(object : FlowCollector<DataState<NoteListViewState>?> {  // listening flow emission
+            override suspend fun emit(value: DataState<NoteListViewState>?) {
+                assert(
+                    value?.stateMessage?.response?.message
+                        ?.contains(CacheErrors.CACHE_ERROR_UNKNOWN)?: false
+                )
+            }
+        })
+
+
+        // confirm cache was not updated
+        val cacheNoteThatWasInserted = noteCacheDataSource.searchNoteById(newNote.id)
+        assertTrue { cacheNoteThatWasInserted == null }
+
+        // confirm network was not updated
+        val networkNoteThatWasInserted = noteNetworkDataSource.searchNote(newNote)
+        assertTrue { networkNoteThatWasInserted == null}
+
+    }
+
 
 
 }
